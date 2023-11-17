38080db5f1dbaa6728b74e02f33266f05ef97d4b
Olakunle Raheem <olaks.raheem@gmail.com>
2023-11-17 21:56:33 +0100

    Some new changes

---
+++ src/components/Boast.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+  import {PersonStanding} from "lucide-vue-next"
+</script>
+
+
+
+<template>
+  <div class="bg-[#002769] flex flex-col gap-10 px-4 py-20 text-white">
+    <p class="text-xl text-yellow-400">
+      Our approach to tutoring is both scientifically and anecdotally considered
+      the best.
+    </p>
+
+    <h5 class="text-4xl font-extrabold">Make us Your Right Channel To Academic excellence and success.</h5>
+
+    <p>
+      We work with you to ensure your kids excel at every stage of their
+      learning journey whether it's building early foundations, helping with
+      homework, getting better grades, mastering their subjects or passing
+      pivotal exams.
+    </p>
+    
+
+
+    <button class="bg-blue-600 rounded-lg px-6 py-4 flex gap-3 justify-center items-center">
+            <PersonStanding /> Get a Professional Tutor
+    </button>
+
+
+    <div class="flex gap-4 flex-wrap justify-center">
+
+        <img src="../assets/student-boy.jpg" alt="student-boy" class="rounded-md h-28 w-28"/>
+        <img src="../assets/student-boy-2.jpg" alt="student-boy-2" class="rounded-md h-56 w-56"/>
+        <img src="../assets/student-girl.jpg" alt="student-girl" class="rounded-md h-28 w-28"/>
+        <img src="../assets/student-boy.jpg" alt="student-boy" class="rounded-md h-28 w-28"/>
+        
+    </div>
+
+
+    <p class="text-xl text-center text-yellow-300">We always deliver!!!</p>
+
+
+    <h5 class="text-2xl font-extrabold text-center">Our students outperform their peers by a massive amount.</h5>
+
+  </div>
+</template>
