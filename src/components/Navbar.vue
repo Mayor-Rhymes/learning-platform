c273b1af6acdcfd46b1b4fbe9b173dffb4c3b692
Olakunle Raheem <olaks.raheem@gmail.com>
2023-12-02 18:50:15 +0100

    Feat: Redesigned navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,7 +18,7 @@ const handleMenuVisibility = () => {
 </script>
 
 <template>
-  <div class="lg:hidden md:hidden flex justify-between items-center px-4">
+  <div class="lg:hidden md:hidden flex justify-between items-center h-[80px]">
     <router-link
       to="/"
       class="lg:hidden md:hidden flex items-center justify-center text-center text-blue-700 text-4xl font-bold"
@@ -33,72 +33,85 @@ const handleMenuVisibility = () => {
 
     <MenuIcon @click="handleMenuVisibility" />
   </div>
-
-  <nav
-    v-if="menuVisible"
-    class="w-full flex flex-col p-2 items-center bg-white justify-center gap-5 font-semibold lg:border-b-gray-100 lg:border-b-4"
+  <Transition
+    enter-active-class="transition duration-100 ease-out"
+    enter-from-class="transform scale-95 opacity-0"
+    enter-to-class="transform scale-100 opacity-100"
+    leave-active-class="transition duration-75 ease-in"
+    leave-from-class="transform scale-100 opacity-100"
+    leave-to-class="transform scale-95 opacity-0"
   >
-    <Menu as="div" class="relative">
-      <MenuButton
-        class="flex items-center hover:bg-gray-200 p-2 rounded"
-        @click="handleVisibility"
-        >Our Services
+    <nav
+      v-if="menuVisible"
+      class="w-full lg:hidden flex flex-col p-2 bg-white justify-center gap-5 font-semibold lg:border-b-gray-100 lg:border-b-4"
+    >
+      <Menu
+        as="div"
+        class="flex items-center gap-2 p-2 justify-center hover:bg-gray-200 hover:rounded-md cursor-pointer transition-all delay-100 duration-150 relative"
+      >
+        <MenuButton
+          class="flex items-center hover:bg-gray-200 p-2 rounded"
+          @click="handleVisibility"
+          >Our Services
+          <transition
+            enter-from-class="transform scale-95 opacity-0"
+            enter-to-class="transform scale-100 opacity-100"
+            leave-from-class="transform scale-100 opacity-100"
+            leave-to-class="transform scale-95 opacity-0"
+          >
+            <ChevronDown v-if="!visible" />
+            <ChevronUp v-else />
+          </transition>
+        </MenuButton>
         <transition
+          enter-active-class="transition duration-100 ease-out"
           enter-from-class="transform scale-95 opacity-0"
           enter-to-class="transform scale-100 opacity-100"
+          leave-active-class="transition duration-75 ease-in"
           leave-from-class="transform scale-100 opacity-100"
           leave-to-class="transform scale-95 opacity-0"
         >
-          <ChevronDown v-if="!visible" />
-          <ChevronUp v-else />
-        </transition>
-      </MenuButton>
-      <transition
-        enter-active-class="transition duration-100 ease-out"
-        enter-from-class="transform scale-95 opacity-0"
-        enter-to-class="transform scale-100 opacity-100"
-        leave-active-class="transition duration-75 ease-in"
-        leave-from-class="transform scale-100 opacity-100"
-        leave-to-class="transform scale-95 opacity-0"
-      >
-        <MenuItems
-          class="inline-flex flex-col gap-5 absolute top-10 w-[400px] rounded-md p-10 shadow-lg bg-white"
-        >
-          <MenuItem
-            class="p-4 rounded-md hover:text-white transition-all delay-150 duration-150"
+          <MenuItems
+            class="inline-flex flex-col gap-5 absolute top-10 w-[400px] rounded-md p-10 shadow-lg bg-white"
           >
-            <router-link
-              to="/tutor"
-              active-class="bg-blue-400"
-              class="hover:bg-blue-500"
+            <MenuItem
+              class="p-4 rounded-md hover:text-white transition-all delay-150 duration-150"
             >
-              Request A Tutor
-            </router-link>
-          </MenuItem>
-        </MenuItems>
-      </transition>
-    </Menu>
+              <router-link
+                to="/tutor"
+                active-class="bg-blue-400"
+                class="hover:bg-blue-500"
+              >
+                Request A Tutor
+              </router-link>
+            </MenuItem>
+          </MenuItems>
+        </transition>
+      </Menu>
 
-    <div
-      class="flex items-center gap-2 p-2 hover:bg-gray-200 hover:rounded-md cursor-pointer transition-all delay-100 duration-150"
-    >
-      <PersonStanding /> Request A Tutor
-    </div>
-    <router-link
-      to="/contact"
-      class="flex items-center gap-2 p-2 hover:bg-gray-200 hover:rounded-md cursor-pointer transition-all delay-100 duration-150"
-    >
-      <Phone /> Contact us
-    </router-link>
+      <router-link
+        to="/tutor"
+        class="flex items-center gap-2 p-2 justify-center hover:bg-gray-200 hover:rounded-md cursor-pointer transition-all delay-100 duration-150"
+      >
+        <PersonStanding /> Request A Tutor
+      </router-link>
+      <router-link
+        active-class="bg-gray-200 rounded-md"
+        to="/contact"
+        class="flex items-center gap-2 p-2 justify-center hover:bg-gray-200 hover:rounded-md cursor-pointer transition-all delay-100 duration-150"
+      >
+        <Phone /> Contact us
+      </router-link>
 
-    <router-link
-      active-class="bg-gray-200 rounded-md"
-      to="/about"
-      class="flex active:bg-gray-200 active:rounded-md items-center p-2 hover:bg-gray-200 hover:rounded-md cursor-pointer transition-all delay-100 duration-150"
-    >
-      About
-    </router-link>
-  </nav>
+      <router-link
+        active-class="bg-gray-200 rounded-md"
+        to="/about"
+        class="flex active:bg-gray-200 justify-center active:rounded-md items-center p-2 hover:bg-gray-200 hover:rounded-md cursor-pointer transition-all delay-100 duration-150"
+      >
+        About
+      </router-link>
+    </nav>
+  </Transition>
 
   <p class="lg:hidden md:hidden text-xl block text-center">
     We Make Learning Superbly Fun and Inclusive
@@ -162,6 +175,7 @@ const handleMenuVisibility = () => {
       <PersonStanding /> Request A Tutor
     </div>
     <router-link
+      active-class="bg-gray-200 rounded-md"
       to="/contact"
       class="flex items-center gap-2 p-2 hover:bg-gray-200 hover:rounded-md cursor-pointer transition-all delay-100 duration-150"
     >
