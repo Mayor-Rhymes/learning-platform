2ca9bf40e3ae63db55489d6d265ad50dd06532a7
Olakunle Raheem <olaks.raheem@gmail.com>
2023-12-22 20:13:19 +0100

    Added logo to testimonials boxes

--- src/components/Testimonial.vue
+++ src/components/Testimonial.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
-   import { defineProps } from 'vue';
+import { defineProps } from "vue";
 
-   defineProps<{content: string; author: string; location?: string}>();
+defineProps<{ content: string; author: string; location?: string }>();
 </script>
 
 <template>
-  <div class="bg-[#F4FDEE] p-7 rounded-md flex flex-col items-center justify-center gap-8">
+  <div
+    class="bg-[#F4FDEE] p-7 rounded-md flex flex-col items-center justify-center gap-8"
+  >
+    <img
+      src="../assets/frithland_logo.jpg"
+      alt="frithland_logo"
+      class="w-40 h-20 object-scale-down self-end"
+    />
     <p class="text-lg font-semibold">
-       {{ content }}
+      {{ content }}
     </p>
 
     <p class="text-md font-medium">{{ author }}</p>
