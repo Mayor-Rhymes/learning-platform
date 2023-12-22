3b3215cbe871b950b37d0776b3bfe7a50bcac81a
Olakunle Raheem <olaks.raheem@gmail.com>
2023-12-22 21:22:09 +0100

    Fixed typo

--- src/components/TestimonialList.vue
+++ src/components/TestimonialList.vue
@@ -3,7 +3,7 @@ import Testimonial from "./Testimonial.vue";
 import { testimonials } from "../lib/mockTestimonials";
 </script>
 <template>
-  <h4 class="text-center text-2xl font-bold">Testimonies</h4>
+  <h4 class="text-center text-2xl font-bold">Testimonials</h4>
   <div class="grid grid-cols-1 lg:grid-cols-3 gap-10 p-5">
     <Testimonial
       v-for="testimonial in testimonials"
